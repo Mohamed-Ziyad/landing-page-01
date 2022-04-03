9809047fc94170ebc04b323b6780ea951cf61e7c
Mohamed-Ziyad <ziyad.web.developer@gmail.com>
2022-04-03 16:40:36 +0530

    Changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./src/App.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.css/i,
-        use: ["style-loader", "css-loader"],
-      },
+	entry: './src/App.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		// publicPath: "/",
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.css/i,
+				use: ['style-loader', 'css-loader'],
+			},
 
-      // {
-      //   test: /\.(png|jpe?g|gif)$/i,
-      //   loader: "file-loader",
-      // },
-    ],
-  },
-  plugins: [new HtmlWebPackPlugin({ template: "./src/index.html" })],
+			// {
+			//   test: /\.(png|jpe?g|gif)$/i,
+			//   loader: "file-loader",
+			// },
+		],
+	},
+	plugins: [new HtmlWebPackPlugin({ template: './src/index.html' })],
 };
